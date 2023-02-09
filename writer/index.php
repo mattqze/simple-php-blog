0f08a9179027cf84fe66f64dc6a3d2b6fb6de7b1
Matt <96447310+mattqze@users.noreply.github.com>
2023-02-09 03:28:52 -0500

    Add files via upload

--- writer/index.php
+++ writer/index.php
@@ -14,8 +14,8 @@ if(!in_array($_SERVER['REMOTE_ADDR'],$iplist)){
 <b>Date</b><br>
 <input type="text" name="date">
 <br><br>
-<b>Content</b><br>
-<textarea id="content" name="content"></textarea> <iframe id="previewframe"></iframe>
+<b>Content</b><br> 
+<textarea id="content" name="content"></textarea><br><br><b>Preview</b><br><iframe id="previewframe"></iframe>
 <br><br>
 <a target="_blank" href="https://developer.mozilla.org/en-US/docs/Learn/HTML/Cheatsheet#inline_elements">Cheat Sheet</a><br><br>
 <a onclick="preview()">Preview</a><br><br>
